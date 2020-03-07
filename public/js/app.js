b9181d3c28b7e3f084d8029c8744d8253e31bb9f
Jadbaltaji <jad_mohammad@hotmail.com>
2020-03-07 02:09:42 -0500

    Added automatic location detecting

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,32 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#msg-1");
 const messagetwo = document.querySelector("#msg-2");
 
+
+
+//GET CURRENT LOCATION
+document.getElementById('button').addEventListener("click", function(e){
+  e.preventDefault()
+  navigator.geolocation.getCurrentPosition((location)=>{
+    var latitude = location.coords.latitude;
+    var longitude = location.coords.longitude;
+    location=longitude+','+latitude
+    messageOne.textContent = "Loading...";
+    messagetwo.textContent = "";
+
+  fetch("/weather?search=" + location).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messagetwo.textContent = data.forecast;
+      }
+    });
+  });
+  })
+})
+ 
+//SEARCH FORM
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
   const location = search.value;
